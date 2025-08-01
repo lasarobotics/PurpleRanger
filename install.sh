3ad9c4b214986710a4885eb3bc05b6949f7280ac
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2025-08-01 01:06:07 -0500

    Install udev rule and pip requirements

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Update system
 sudo apt-get update
 sudo apt-get upgrade --yes
+
+# Install dependencies
 sudo apt-get install --yes zip unzip git build-essential libgl1 python3-venv python3-pip python-is-python3
 
+# Enable user service linger
+sudo loginctl enable-linger $USER
+
+# Install udev rule
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+# Copy service file
 mkdir --parents ~/.config/systemd/user
 cp systemd/PurpleRanger@.service ~/.config/systemd/user
-loginctl enable-linger $USER
 systemctl --user daemon-reload
+
+# Create Python venv
+python -m venv .venv
+.venv/bin/pip install -r requirements.txt
 
 echo
 echo "To use, enable the instance service you would like, using 'systemctl --user enable PurpleRanger@<pipeline_name>.service'"
